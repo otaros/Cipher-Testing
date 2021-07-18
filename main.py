6026f573d411c2e40a4fc0b50d13e95cbcddb30d
Khang <72249219+otaros@users.noreply.github.com>
2021-07-18 21:08:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 def encrypt(target: str, decode1: str, decode2: str, base_step: int, reset_step: int) -> str:
-    step = base_step
+    #processing value
+    step = base_step #save the step for the reset
     result = ""
     shift = 0
-    count = 0
+    count = 0       #counting for the reset
     for i in target:
-        if i == " ":
+        if i == " ":        #if the char is space just skip
             result += " "
-            continue
-        if count == reset_step:
+            continue        #skip to next char
+        if count == reset_step:     #check reset
             count = 0
             shift += 1
             step = base_step
-        if shift % 2 == 1 and shift != 0:
+        if shift % 2 == 1 and shift != 0:       #shifting the decode2 string
             decode2 += decode2[0]
             decode2 = decode2[1:len(decode2)]
-        elif shift % 2 == 0 and shift != 0:
+        elif shift % 2 == 0 and shift != 0:     #shifting the decode1 string
             decode1 = decode1[len(decode1) - 1:len(decode1)] + decode1[0:len(decode1) - 1]
-        pos = decode1.find(i)
-        pos += step
-        if (pos > len(decode2) - 1):
+        pos = decode1.find(i)           #take position
+        pos += step                     #plus step
+        if (pos > len(decode2) - 1):    #roll back if pos is out of decode2 range
             pos = len(decode2) - pos
-        result += decode2[pos]
-        step += 1
-        count += 1
+        result += decode2[pos]          #take the encrypted char
+        step += 1                       #increase the step
+        count += 1                      #counting to reset
     return result
 
 
