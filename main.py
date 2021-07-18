6348215ea22d92f26fdb1762d072b33887cd58f6
Khang <72249219+otaros@users.noreply.github.com>
2021-07-18 21:01:54 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,66 @@
+def encrypt(target: str, decode1: str, decode2: str, base_step: int, reset_step: int) -> str:
+    step = base_step
+    result = ""
+    shift = 0
+    count = 0
+    for i in target:
+        if i == " ":
+            result += " "
+            continue
+        if count == reset_step:
+            count = 0
+            shift += 1
+            step = base_step
+        if shift % 2 == 1 and shift != 0:
+            decode2 += decode2[0]
+            decode2 = decode2[1:len(decode2)]
+        elif shift % 2 == 0 and shift != 0:
+            decode1 = decode1[len(decode1) - 1:len(decode1)] + decode1[0:len(decode1) - 1]
+        pos = decode1.find(i)
+        pos += step
+        if (pos > len(decode2) - 1):
+            pos = len(decode2) - pos
+        result += decode2[pos]
+        step += 1
+        count += 1
+    return result
+
+
+def decrypt(target: str, decode1: str, decode2: str, base_step: int, reset_step:int) -> str:
+    step = base_step
+    result = ""
+    shift = 0
+    count = 0
+    for i in target:
+        if i == " ":
+            result += " "
+            continue
+        if count == reset_step:
+            count = 0
+            shift += 1
+            step = base_step
+        if shift % 2 == 1 and shift != 0:
+            decode2 += decode2[0]
+            decode2 = decode2[1:len(decode2)]
+        elif shift % 2 == 0 and shift != 0:
+            decode1 = decode1[len(decode1) - 1:len(decode1)] + decode1[0:len(decode1) - 1]
+        pos = decode2.find(i)
+        pos -= step
+        if (pos < 0):
+            pos = len(decode1) + pos
+        result += decode1[pos]
+        step += 1
+        count += 1
+    return result
+
+
+str1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+str2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+target = "HELLO MOTHERFUCKER"
+init_step = 1
+reset_step = 4
+print(target)
+en = encrypt(target, str1, str2, init_step,reset_step)
+print(en)
+de = decrypt(en, str1, str2, init_step,reset_step)
+print(de)
